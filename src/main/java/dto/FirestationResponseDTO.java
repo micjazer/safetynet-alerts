47b52400cc90b790cbbf276e5ada22f21b13641f
Michel Jazeron <michel.jazeron.com>
2025-03-26 14:35:22 +0100

    C13 Ajout du code dans FirestationResponseDTO.java

--- src/main/java/dto/FirestationResponseDTO.java
+++ src/main/java/dto/FirestationResponseDTO.java
@@ -1,4 +1,17 @@
-package dto;
+package com.safetynet.alerts.dto;
+
+import java.util.List;
 
 public class FirestationResponseDTO {
+    private List<PersonSummaryDTO> persons;
+    private int adults;
+    private int children;
+
+    public FirestationResponseDTO(List<PersonSummaryDTO> persons, int adults, int children) {
+        this.persons = persons;
+        this.adults = adults;
+        this.children = children;
+    }
+
+    // Getters and Setters
 }
