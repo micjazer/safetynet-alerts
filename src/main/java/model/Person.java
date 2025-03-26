e7ff2c0981b16981dfd15e70e8de53f7dafdddb2
Michel Jazeron <michel.jazeron.com>
2025-03-26 14:49:18 +0100

    C17 Ajout du code dans Person.java

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,4 +1,13 @@
-package model;
+package com.safetynet.alerts.model;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String zip;
+    private String phone;
+    private String email;
+
+    // Getters and Setters
 }
