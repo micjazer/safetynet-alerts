79d9b13a7374b14a0289f3bd8678819278982070
Michel Jazeron <michel.jazeron.com>
2025-03-26 14:33:10 +0100

    C12 Ajout du code dans FirestationController.java

--- src/main/java/controller/FirestationController.java
+++ src/main/java/controller/FirestationController.java
@@ -1,4 +1,25 @@
-package controller;
+package com.safetynet.alerts.controller;
 
+import com.safetynet.alerts.dto.FirestationResponseDTO;
+import com.safetynet.alerts.service.FirestationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/firestation")
 public class FirestationController {
+    private static final Logger log = LoggerFactory.getLogger(FirestationController.class);
+    private final FirestationService service;
+
+    public FirestationController(FirestationService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<FirestationResponseDTO> getByStation(@RequestParam int stationNumber) {
+        log.info("GET /firestation?stationNumber={}", stationNumber);
+        return ResponseEntity.ok(service.getPersonsByStation(stationNumber));
+    }
 }
