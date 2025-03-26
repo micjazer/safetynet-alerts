f99bece2c8b086289b6869c0407ba108e463f811
Michel Jazeron <michel.jazeron.com>
2025-03-26 14:58:04 +0100

    C20 Ajout du code dans AgeUtils.java

--- src/main/java/utils/AgeUtils.java
+++ src/main/java/utils/AgeUtils.java
@@ -1,4 +1,21 @@
-package utils;
+package com.safetynet.alerts.utils;
+
+import com.safetynet.alerts.model.MedicalRecord;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class AgeUtils {
+    public static int calculateAge(String firstName, String lastName, List<MedicalRecord> records) {
+        return records.stream()
+                .filter(mr -> mr.getFirstName().equals(firstName) && mr.getLastName().equals(lastName))
+                .findFirst()
+                .map(mr -> {
+                    LocalDate birthdate = LocalDate.parse(mr.getBirthdate(), DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+                    return Period.between(birthdate, LocalDate.now()).getYears();
+                })
+                .orElse(0);
+    }
 }
