d276fcb26587d8b1ab040b1947498070fb0857c3
Michel Jazeron <michel.jazeron.com>
2025-03-26 14:47:35 +0100

    C16 Ajout du code dans MedicalRecord.java

--- src/main/java/model/MedicalRecord.java
+++ src/main/java/model/MedicalRecord.java
@@ -1,4 +1,13 @@
-package model;
+package com.safetynet.alerts.model;
+
+import java.util.List;
 
 public class MedicalRecord {
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+    private List<String> medications;
+    private List<String> allergies;
+
+    // Getters and Setters
 }
