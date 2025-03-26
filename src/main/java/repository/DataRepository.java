0352acff470678ad0738d12ec70853b0c21fb115
Michel Jazeron <michel.jazeron.com>
2025-03-26 14:51:42 +0100

    C18 Ajout du code dans DataRepository.java

--- src/main/java/repository/DataRepository.java
+++ src/main/java/repository/DataRepository.java
@@ -1,4 +1,33 @@
-package repository;
+package com.safetynet.alerts.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.alerts.model.*;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
 public class DataRepository {
+    private List<Person> persons;
+    private List<Firestation> firestations;
+    private List<MedicalRecord> medicalRecords;
+
+    @PostConstruct
+    public void init() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("data.json");
+        JsonNode root = mapper.readTree(is);
+        persons = Arrays.asList(mapper.treeToValue(root.get("persons"), Person[].class));
+        firestations = Arrays.asList(mapper.treeToValue(root.get("firestations"), Firestation[].class));
+        medicalRecords = Arrays.asList(mapper.treeToValue(root.get("medicalrecords"), MedicalRecord[].class));
+    }
+
+    public List<Person> getPersons() { return persons; }
+    public List<Firestation> getFirestations() { return firestations; }
+    public List<MedicalRecord> getMedicalRecords() { return medicalRecords; }
 }
