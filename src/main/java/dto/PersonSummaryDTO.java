2a5247062dc167c01fc9887d6bfed4af1ba3f408
Michel Jazeron <michel.jazeron.com>
2025-03-26 14:37:48 +0100

    C14 Ajout du code dans PersonSummaryDTO.java

--- src/main/java/dto/PersonSummaryDTO.java
+++ src/main/java/dto/PersonSummaryDTO.java
@@ -1,4 +1,17 @@
-package dto;
+package com.safetynet.alerts.dto;
 
 public class PersonSummaryDTO {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+
+    public PersonSummaryDTO(String firstName, String lastName, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    // Getters and Setters
 }
